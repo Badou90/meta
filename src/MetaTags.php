85d44fb0705a2c7fd3ba8247cc759d7d2891b044
Vyacheslav Goriachikh <badou@yandex.ru>
2016-11-10 16:18:46 +0900

    Changed whole component

--- src/MetaTags.php
+++ src/MetaTags.php
@@ -3,34 +3,15 @@ namespace Badou\MetaTags;
 
 class MetaTags
 {
-    protected $title;
-    protected $keywords;
-    protected $description;
+    protected $tags = [];
 
-    public function title()
+    public function set($name, $value)
     {
-        $title = ($this->title)?$this->title:config('metatags.title.default');
-
-        if($this->title)
-            $title = config('metatags.title.prepend').config('metatags.title.separator').$title.config('metatags.title.separator').config('metatags.title.append');
-
-        return "<title>{$title}</title>";
+        $this->$tags[$name] = $value;
     }
 
-    public function keywords()
+    public function tag($name)
     {
-        $keywords = ($this->keywords)?$this->keywords:config('metatags.keywords.default');
-        return "<meta name='keywords' content='{$keywords}'>";
-    }
-
-    public function description()
-    {
-        $description = ($this->description)?$this->description:config('metatags.description.default');
-        return "<meta name='description' content='{$description}'>";
-    }
-
-    public function set($type, $value)
-    {
-        $this->$type = $value;
+        return $this->tags[$name];
     }
 }
